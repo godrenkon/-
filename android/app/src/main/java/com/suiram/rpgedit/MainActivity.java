430577e8aec34202d769e16d625d19f4515b2cae
godrenkon <renkomkun@gmail.com>
2026-09-13 20:24:43 +0900

    release: v1.1.0 native desktop and Android builds

---
+++ android/app/src/main/java/com/suiram/rpgedit/MainActivity.java
@@ -0,0 +1,5 @@
+package com.suiram.rpgedit;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.suiram.rpgedit',
+  appName: 'Suiram RPG Edit',
+  webDir: 'dist',
+  android: { allowMixedContent: false },
+};
+
+export default config;
